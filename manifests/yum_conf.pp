b181995bce38bc35d991179cded2cc3d7133b0c0
Stefan Goethals <stefan@zipkid.eu>
2014-03-28 12:28:21 +0100

    manage proxy stuff better..

--- manifests/yum_conf.pp
+++ manifests/yum_conf.pp
@@ -1,22 +1,32 @@
 class repo::yum_conf ( 
-  $proxy_host, 
+  $proxy_host = undef, 
   $proxy_port = 80, 
+  $proxy_address = undef,
   $proxy_proto = 'http', 
   $proxy_user = undef, 
   $proxy_pass = undef, 
   $proxy_ensure = 'present' 
 ) {
 
-  if $proxy_ensure == 'present' {
-    $yum_proxy_action = 'set'
-  } else {
-    $yum_proxy_action = 'rm'
+  if $proxy_host or $proxy_address {
+    #fail('Class repo::yum_conf needs either $proxy_host or $proxy_address set.')
+
+    if ! $proxy_address {
+      $proxy_string = "${proxy_host}:${proxy_port}"
+    } else {
+      $proxy_string = $proxy_address
+    }
+
+    if $proxy_ensure == 'present' {
+      $yum_proxy_action = 'set'
+    } else {
+      $yum_proxy_action = 'rm'
+    }
+
+    augeas {'yum.conf.main.proxy':
+      context => "/files/etc/yum.conf/main",
+      changes => [ "${yum_proxy_action} proxy ${proxy_proto}://${proxy_string}", ],
+    } 
   }
-
-  augeas {'yum.conf.main.proxy':
-    context => "/files/etc/yum.conf/main",
-    changes => [ "${yum_proxy_action} proxy ${proxy_proto}://${proxy_host}:${proxy_port}", ],
-  } 
-
 
 }
