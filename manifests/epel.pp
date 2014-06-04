dcd4fb851e662b0513f632b0b5c0a91460bc2c19
Stefan Goethals <stefan@zipkid.eu>
2014-06-04 16:36:54 +0200

    epel for RH 5

--- manifests/epel.pp
+++ manifests/epel.pp
@@ -4,9 +4,21 @@ class repo::epel (
   $ensure = 'present',
 ) {
 
-  file{'/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6':
+  case $::os_maj_version {
+    '4': {
+      fail( 'Version 4 not supported!')
+    }
+    '5': {
+      $keyfile = 'RPM-GPG-KEY-EPEL'
+    }
+    default: {
+      $keyfile = "RPM-GPG-KEY-EPEL-${::os_maj_version}"
+    }
+  }
+
+  file{"/etc/pki/rpm-gpg/${keyfile}":
     ensure => $ensure,
-    source => 'puppet:///modules/repo/keys/RPM-GPG-KEY-EPEL-6',
+    source => "puppet:///modules/repo/keys/${keyfile}",
   }
 
   file{'/etc/yum.repos.d/epel.repo':
