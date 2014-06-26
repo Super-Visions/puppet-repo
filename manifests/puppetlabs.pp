26610ef3729e6e9c537265554c623c0626f688bf
Stefan Goethals <stefan@zipkid.eu>
2014-06-26 11:42:36 +0200

    Just 1...

--- manifests/puppetlabs.pp
+++ manifests/puppetlabs.pp
@@ -1,6 +1,7 @@
 class repo::puppetlabs (
   $proxy = undef,
   $ensure = 'present',
+  $baseurl = 'http://yum.puppetlabs.com/el/$releasever/products/$basearch',
 ) {
 
   file{'/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs':
