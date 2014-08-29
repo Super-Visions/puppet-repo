43f30cce4a8b3d8e5f034055eda08ccd561bd2f0
Christophe Fonteyne <christophe.fonteyne@super-visions.com>
2014-08-29 15:18:16 +0200

    proxy support for rhn_mobistar and super_visions_mobistar

--- manifests/rhn_mobistar.pp
+++ manifests/rhn_mobistar.pp
@@ -4,7 +4,7 @@ class repo::rhn_mobistar  (
 
   file{'/etc/yum.repos.d/rhn-mobistar.repo':
     ensure => $ensure,
-    source => 'puppet:///modules/repo/repos/rhn-mobistar.repo',
+    content => template('repo/repos/rhn-mobistar.repo.erb'),
   }
 
 }
--- manifests/super_visions_mobistar.pp
+++ manifests/super_visions_mobistar.pp
@@ -4,7 +4,7 @@ class repo::super_visions_mobistar (
 
   file{'/etc/yum.repos.d/super-visions-mobistar.repo':
     ensure => $ensure,
-    source => 'puppet:///modules/repo/repos/super-visions-mobistar.repo',
+    content => template('repo/repos/super-visions-mobistar.repo.erb'),
   }
 
 }
