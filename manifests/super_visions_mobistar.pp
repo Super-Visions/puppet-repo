2f7467b743e2bf955b10948a205ebf64b6a2ce7b
Stefan Goethals <stefan@zipkid.eu>
2014-09-01 16:49:56 +0200

    proxy

--- manifests/super_visions_mobistar.pp
+++ manifests/super_visions_mobistar.pp
@@ -1,9 +1,13 @@
+#
+# Internal repo
+#
 class repo::super_visions_mobistar (
   $ensure = 'present',
-  ) {
+  $proxy = undef,
+) {
 
   file{'/etc/yum.repos.d/super-visions-mobistar.repo':
-    ensure => $ensure,
+    ensure  => $ensure,
     content => template('repo/repos/super-visions-mobistar.repo.erb'),
   }
 
